a6ef7b3ab71a48ffbb6c1ee1619371b5f35febb0
Kailash Soni <kailash906@outlook.com>
2019-04-24 19:39:42 +0400

    changed backend to online

--- src/helpers/network.js
+++ src/helpers/network.js
@@ -2,7 +2,7 @@ import { getToken } from "./authentication";
 
 function loginRequest({ email, password }) {
   return new Promise((resolve, reject) => {
-    fetch("http://localhost:5000/users/login", {
+    fetch("https://astrobackend.herokuapp.com/users/login", {
       method: "post",
       headers: {
         "Content-Type": "application/json; charset=utf-8"
@@ -24,7 +24,7 @@ function loginRequest({ email, password }) {
 
 function registerRequest({ name, email, password }) {
   return new Promise((resolve, reject) => {
-    fetch("http://localhost:5000/users/register", {
+    fetch("https://astrobackend.herokuapp.com/users/register", {
       method: "post",
       headers: {
         "Content-Type": "application/json; charset=utf-8"
@@ -47,13 +47,13 @@ function registerRequest({ name, email, password }) {
 
 function getDashboard() {
   return new Promise((resolve, reject) => {
-    fetch("http://localhost:5000/dashboard", {
+    fetch("https://astrobackend.herokuapp.com/dashboard", {
       headers: {
         'Authorization': getToken()
       }
     }).then(response => {
       if (!response.ok) {
-        reject(new Error("Unathorized"));
+        reject(new Error("Unauthorized"));
         return;
       }
       response.json().then(json => {
